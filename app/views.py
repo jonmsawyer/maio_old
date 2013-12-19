1339ff97ae7879a62a98c1a079cde48a9cd4c568
Jonathan Sawyer <jmsawyer@alaska.edu>
2013-12-19 00:33:51 -0900

    You can now scrub according to the random list. A previous random image shows up in order.

--- app/views.py
+++ app/views.py
@@ -18,6 +18,19 @@ def __load_session_query(request, query_set):
     query_set.query = pickle.loads(str(current_query))
     return query_set
 
+def __load_random_list(request, query):
+    try:
+        id_list = request.session['random_list']
+    except:
+        ob_list = list(query.values_list('id', flat=True))
+        id_list = []
+        while len(ob_list) is not 0:
+            rand_max = len(ob_list)
+            rand_min = 0
+            id_list.append(ob_list.pop(random.randrange(rand_min, rand_max)))
+        request.session['random_list'] = id_list
+    return id_list
+
 def home(request):
     context = {}
     return render(request, 'home.html', context)
@@ -29,10 +42,19 @@ def get_file(request, id=None):
         f_data = fh.read()
     response = HttpResponse(f_data, content_type=f.mime_type)
     response['Content-Disposition'] = 'attachment; filename="%s"' % (f.file_path.split('/')[-1],)
+    try:
+        f.views = f.views + 1
+        f.save()
+    except:
+        raise
     return response
 
 def images_index(request):
     query = __store_session_query(request, File.get_all_images())
+    try:
+        del request.session['random_list']
+    except KeyError:
+        pass
     context = {'files': query}
     return render(request, 'images/index.html', context)
 
@@ -56,14 +78,12 @@ def images_getthis(request, id):
     response = HttpResponse(json.dumps(context), content_type='application/json')
     return response
 
-def images_getrandom(request, id):
+def images_getrandom_next(request, id):
     query = __load_session_query(request, File.objects.all())
-    id_list = list(query.values_list('id', flat=True))
+    id_list = __load_random_list(request, query)
     try:
-        rand_max = len(id_list)
-        rand_min = 0
-        next_id = id_list[random.randrange(rand_min, rand_max)]
-        number = id_list.index(id) + 1
+        next_id = id_list[id_list.index(id) + 1]
+        number = id_list.index(id) + 2
     except IndexError:
         # does not take into account a list of size 0, todo: test!
         next_id = id_list[0]
@@ -77,6 +97,36 @@ def images_getrandom(request, id):
         'name': obj.file_path.split('/')[-1],
     }
     response = HttpResponse(json.dumps(context), content_type='application/json')
+    return response
+
+def images_getrandom_prev(request, id):
+    query = __load_session_query(request, File.objects.all())
+    id_list = __load_random_list(request, query)
+    try:
+        next_id = id_list[id_list.index(id) - 1]
+        number = id_list.index(id)
+    except IndexError:
+        # does not take into account a list of size 0, todo: test!
+        next_id = id_list[-1]
+        number = len(id_list)
+
+    obj = File.objects.get(id=next_id)
+    context = {
+        'id': obj.id,
+        'number': number,
+        'count': len(id_list),
+        'name': obj.file_path.split('/')[-1],
+    }
+    response = HttpResponse(json.dumps(context), content_type='application/json')
+    return response
+
+def images_unset_random(request):
+    try:
+        del request.session['random_list']
+    except KeyError:
+        pass
+    msg = {'ok': 'deleted random list'}
+    response = HttpResponse(json.dumps(msg), content_type='application/json')
     return response
 
 def images_getnext(request, id):
